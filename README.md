7cfcac75575746caf15413f27e78b96403aafaa3
Mia Wang <57921817+miaw2019@users.noreply.github.com>
2020-03-06 15:22:57 -0800

    initiate

---
+++ README.md
@@ -0,0 +1,76 @@
+Starbuck_Capstone
+==============================
+
+My goal of this project is to build a predict model to find out whether a customer will respond to an certain offer based on the simulated. The model I choose is LighGBM. 
+
+Folder Structures
+------------
+
+    ├── LICENSE
+    ├── Makefile           <- Makefile with commands like `make data` or `make train`
+    ├── README.md          <- The top-level README for developers using this project.
+    ├── data
+    │   ├── external       <- Data from third party sources.
+    │   ├── interim        <- Intermediate data that has been transformed.
+    │   ├── processed      <- The final, canonical data sets for modeling.
+    │   └── raw            <- The original, immutable data dump.
+    │
+    ├── docs               <- A default Sphinx project; see sphinx-doc.org for details
+    │
+    ├── models             <- Trained and serialized models, model predictions, or model summaries
+    │
+    ├── notebooks          <- Jupyter notebooks. Naming convention is a number (for ordering),
+    │                         the creator's initials, and a short `-` delimited description, e.g.
+    │                         `1.0-jqp-initial-data-exploration`.
+    │
+    ├── references         <- Data dictionaries, manuals, and all other explanatory materials.
+    │
+    ├── reports            <- Generated analysis as HTML, PDF, LaTeX, etc.
+    │   └── figures        <- Generated graphics and figures to be used in reporting
+    │
+    ├── requirements.txt   <- The requirements file for reproducing the analysis environment, e.g.
+    │                         generated with `pip freeze > requirements.txt`
+    │
+    ├── setup.py           <- makes project pip installable (pip install -e .) so src can be imported
+    ├── src                <- Source code for use in this project.
+    │   ├── __init__.py    <- Makes src a Python module
+    │   │
+    │   ├── data           <- Scripts to download or generate data
+    │   │   └── make_dataset.py
+    │   │
+    │   ├── features       <- Scripts to turn raw data into features for modeling
+    │   │   └── build_features.py
+    │   │
+    │   ├── models         <- Scripts to train models and then use trained models to make
+    │   │   │                 predictions
+    │   │   ├── predict_model.py
+    │   │   └── train_model.py
+    │   │
+    │   └── visualization  <- Scripts to create exploratory and results oriented visualizations
+    │       └── visualize.py
+    │
+    └── tox.ini            <- tox file with settings for running tox; see tox.testrun.org
+
+
+--------
+
+<p><small>Project based on the <a target="_blank" href="https://drivendata.github.io/cookiecutter-data-science/">cookiecutter data science project template</a>. #cookiecutterdatascience</small></p>
+
+Results
+------------
+
+After several round of tunning the result of RMSE for test data reaches 0.000556 and the log_loss is lower than 0.000166. From the model we can tell that the top 3 important features for split are:
+
+1.Customers' income
+
+2.Offer view time
+
+3.The year of the customer becomes the member
+
+the top 5 important features for gain are:
+
+1.whether the offer is complete
+
+2.whether the offer is viewed
+
+3.Offer view time
---
+++ notebooks/.ipynb_checkpoints/helper-checkpoint.py
@@ -0,0 +1,99 @@
+import pandas as pd
+from datetime import datetime
+from sklearn.preprocessing import MultiLabelBinarizer,OneHotEncoder
+
+def process_portfolio(path):
+    """Read and process portfolio data
+    
+    Arguments:
+        path {str} -- the path of portfolio data
+    """    
+    portfolio_raw = pd.read_json(path, orient='records', lines=True)
+
+    # Fix Column name
+    portfolio_raw.rename(columns={"id": "offer_id"},inplace=True)
+
+    # create a dataframe of the 4 individual channel columns
+    mlb = MultiLabelBinarizer()
+    mlb.fit(portfolio_raw['channels'])
+    offer_channels = pd.DataFrame(mlb.transform(portfolio_raw['channels']),
+        columns=mlb.classes_)
+
+    # drop the original channels column from `portfolio_raw`
+    portfolio = portfolio_raw.drop('channels',axis=1)
+
+    # concatenate the original dataframe with the new `offer_channels` dataframe
+    portfolio = pd.concat([portfolio, offer_channels], axis = 1)
+
+    return portfolio
+
+def process_profile(path):
+    """Read and process profile data 
+
+    Arguments:
+        path {str} -- the path of profile data
+    """    
+    profile = pd.read_json(path, orient='records', lines=True)
+
+    # Fix Column name
+    profile.rename(columns={"id": "customer_id"},inplace=True)
+
+    # Drop custpmers with missing data.
+    profile = profile[profile['gender'].notnull()]
+    profile = profile[profile['income'].notnull()]
+    profile = profile[profile.age != 118]
+
+    # Convert column data type
+    profile.became_member_on = pd.to_datetime(profile.became_member_on,format='%Y%m%d')
+    profile.replace({'gender': {'F': 0, 'M': 1, 'O': 2}}, inplace=True)
+    profile = profile.astype({'gender':'int32'})
+    profile = profile[profile['gender']!=2]
+
+    # Add calculated column
+    profile['became_member_on_dayofweek'] = profile.became_member_on.dt.dayofweek
+    profile['became_member_on_year'] = profile.became_member_on.dt.year
+    profile['became_member_on_month'] = profile.became_member_on.dt.month
+        
+    for i in range(11):
+        if i != 0:
+            stage = i * 10
+            profile[str(stage)+"'s"] = profile['age'].apply(lambda x:1 if((x-stage<10)and (x-stage>=0)) else 0)
+    
+    return profile
+
+def process_transcript(path):
+    """Read and process transcript data
+    
+    Arguments:
+        path {str} -- the path of transcript data
+    """    
+    transcript = pd.read_json(path, orient='records', lines=True)
+    # create a dataframe of the 3 individual value columns
+    values_df = pd.DataFrame(transcript.value.tolist())
+    values_df['offer id'].fillna(values_df['offer_id'],inplace=True)
+    values_df.drop('offer_id', axis=1,inplace=True)
+    values_df.rename(columns={"offer id": "offer_id"},inplace=True)
+
+    # concatenate the original dataframe with the new `offer_channels` dataframe
+    transcript = pd.concat([transcript, values_df], axis = 1)
+
+    # drop the original channels column from `portfolio`
+    transcript = transcript.drop('value',axis=1)
+
+    # change person to customer_id
+    transcript.rename(columns={"person": "customer_id"},inplace=True)
+
+    # change hour to day
+    transcript['time'] /= 24.0
+
+    # One hot encoding event column
+    offer_event =  pd.get_dummies(transcript['event'])
+    transcript = pd.concat([transcript, offer_event], axis = 1)
+    # transcript = transcript.drop('event',axis=1)
+    transcript.rename(columns={"offer received": "received",
+                            "offer viewed":"viewed",
+                            "offer completed":"completed"
+                            },inplace=True)
+    offer = transcript[transcript.event.isin(['offer received','offer viewed','offer completed'])][['event','offer_id','customer_id','time','received','viewed','completed']]
+    transaction = transcript[transcript.event =='transaction' ][['customer_id','time','amount']]
+    return offer,transaction
